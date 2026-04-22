9a8a34e0716b0c0e11bbe232225afdfa22e2076b
PhilippGrubmann <grubmann.philipp@gmail.com>
2026-04-22 11:54:08 +0200

    encoder.c: Buffer + Winkelvariablen angelegt

--- Core/Inc/encoder.h
+++ Core/Inc/encoder.h
@@ -8,6 +8,17 @@
 #ifndef INC_ENCODER_H_
 #define INC_ENCODER_H_
 
+#define ENCODER_POLE_PAIRS    4
+/* 3/5 Vdd = 3V * 15/22 = 2,045V
+ * ADC_OFFSET = 2,045 * 4095 / 3,3 = 2537
+ */
+#define ENCODER_ADC_OFFSET       2537
+#define ENCODER_ADC_AMPLITUDE    931
+
+void  encoder_init(void);
+void  encoder_update(void);
+float encoder_get_angle_mech(void);
+float encoder_get_angle_elec(void);
 
 
 #endif /* INC_ENCODER_H_ */
--- Core/Src/encoder.c
+++ Core/Src/encoder.c
@@ -5,4 +5,14 @@
  *      Author: grubm
  */
 
+#include "encoder.h"
+#include <math.h>
+#include "stm32f4xx_hal.h"
+
+extern ADC_HandleTypeDef hadc3;
+
+// adc3_buffer[0] = Sinus  (PF6, ADC3_IN4)
+// adc3_buffer[1] = Cosinus (PF7, ADC3_IN5)
+static uint16_t adc3_buffer[2];
+
 
