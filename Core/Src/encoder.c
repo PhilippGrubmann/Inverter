d1cd0a499e1eb8250175cf3517c45023532720fe
PhilippGrubmann <grubmann.philipp@gmail.com>
2026-04-22 11:54:37 +0200

    encoder.c: Buffer + Winkelvariablen angelegt2

--- Core/Src/encoder.c
+++ Core/Src/encoder.c
@@ -8,11 +8,15 @@
 #include "encoder.h"
 #include <math.h>
 #include "stm32f4xx_hal.h"
+#include "main.h"
 
 extern ADC_HandleTypeDef hadc3;
 
 // adc3_buffer[0] = Sinus  (PF6, ADC3_IN4)
 // adc3_buffer[1] = Cosinus (PF7, ADC3_IN5)
 static uint16_t adc3_buffer[2];
+
+static float angle_mech_rad = 0.0f;   // Mechanischer Winkel [0, 2π)
+static float angle_elec_rad = 0.0f;   // Elektrischer Winkel [0, 2π)
 
 
